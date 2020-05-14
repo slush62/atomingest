14fb2009a2d4b5dbbed9442d724ff2b91003f9b4
slush62 <steve@lushfamily.com>
2020-05-14 09:05:49 -0500

    Initial spring.io

---
+++ src/main/java/com/peraton/wmss/nws/atomingest/AtomingestApplication.java
@@ -0,0 +1,13 @@
+package com.peraton.wmss.nws.atomingest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtomingestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtomingestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peraton/wmss/nws/atomingest/AtomingestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peraton.wmss.nws.atomingest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtomingestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
